192bc8e3739412b1457f56f7c15aa0c309c1f385
SimpleDioney <62361235+SimpleDioney@users.noreply.github.com>
2022-07-24 12:58:19 -0300

    Updated estilos.cssscript.js

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ localStorage.setItem('theme', 'dark');
 //acessado o tema da maquina do usuário
 localStorage.getItem('theme');
 // dark
+const btn = document.getElementById("toggle");
+const refresh = document.getElementById("refresh");
+const theme = window.localStorage.getItem("theme");
+
+/* verifica se o tema armazenado no localStorage é escuro
+se sim aplica o tema escuro ao body */
+if (theme === "dark") document.body.classList.add("dark");
+
+// event listener para quando o botão de alterar o tema for clicado
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (theme === "dark") {
+    window.localStorage.setItem("theme", "light");
+  } else window.localStorage.setItem("theme", "dark");
+});
+
+refresh.addEventListener("click", () => {
+  window.location.reload();
+});
