fc9ac86a0e11aa960a032fc51399b78bee9a8cd8
SimpleDioney <62361235+SimpleDioney@users.noreply.github.com>
2022-07-24 12:53:34 -0300

    Updated estilos.cssscript.js

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-// warningfunction darkMode() {
-//  const body = document.body
-//  body.classList.toggle('dark-mode')
-//  const btn = document.querySelector('#btn')
-//  const nomeb = document.querySelector('#nomeb')
-//  nomeb.classList.toggle('dark-mode-n')
-//
-
-const theme = {
-  light: {
-    background: 'white',
-    color: 'black',
-  },
-  dark: {
-    background: '#111B21',
-    color: 'darkgrey',
-  }
-};
-function setTheme(newTheme) {
-  const themeColors = themes[newTheme]; // Seleciona o tema para aplicar
-
-  Object.keys(themeColors).map(function(key) {
-    body.style.setProperty(`--${key}`, themeColors[key]); // Altera as variáveis no css
-  });
+function darkMode() {
+  const body = document.body
+  body.classList.toggle('dark-mode')
+  const btn = document.querySelector('#btn')
+  btn.classList.toggle('dark-mode-btn')
+  const nomeb = document.querySelector('#nomeb')
+  nomeb.classList.toggle('dark-mode-n')
 }
-const darkModeToggle = document.querySelector('input[name=theme]');
-darkModeToggle.addEventListener('change', function({ target }) {
-  setTheme(target.checked ? 'dark' : 'light');
-});
