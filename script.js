39642af76ee46a381ea3702145098672ca6ae2ca
SimpleDioney <62361235+SimpleDioney@users.noreply.github.com>
2022-07-24 12:42:17 -0300

    Updated script.js.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function darkMode() {
-  const body = document.body
-  body.classList.toggle('dark-mode')
-  const btn = document.querySelector('#btn')
-  btn.classList.toggle('dark-mode-btn')
-  const nomeb = document.querySelector('#nomeb')
-  nomeb.classList.toggle('dark-mode-n')
-}
+// warningfunction darkMode() {
+//  const body = document.body
+//  body.classList.toggle('dark-mode')
+//  const btn = document.querySelector('#btn')
+//  const nomeb = document.querySelector('#nomeb')
+//  nomeb.classList.toggle('dark-mode-n')
+//
+
+const temas = {
+  light: {
+    background: 'white',
+    color: 'black',
+  },
+  dark: {
+    background: '#111B21',
+    color: 'darkgrey',
+  }
+};
